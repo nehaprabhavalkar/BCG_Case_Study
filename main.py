8281e968fe7af4de5d939f21f1b9bc5936cd5c72
Neha Prabhavalkar <62091070+nehaprabhavalkar@users.noreply.github.com>
2022-12-02 16:27:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from app.run import run
 import sys 
 import ast 
 
-# spark-submit main.py "{'job_name': 'analyze_data', 'path': ''}"
+# spark-submit main.py "{'job_name': 'analyze_data', 'path': 'app/conf/config.yaml'}"
 
 if __name__ == "__main__":
     str_params = sys.argv[1]
